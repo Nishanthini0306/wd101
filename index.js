ed65d29ccaa21ca8602cb3f7bbfbea0ab386ed11
Nishanthini0306 <141767345+Nishanthini0306@users.noreply.github.com>
2023-08-09 00:07:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,73 @@
-[11:42 pm, 08/08/2023] Anu Fi: <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css" />
-</head>
-<body class="flex items-center justify-center ">
-    <div class=" flex flex-col justify-center items-center w-1/2 h-3/4 border-4">
-        <form id="form" class="flex items-center justify-around flex-col" action="">
-            <h1 class="font-extrabold text-3xl">Registration</h1>
-            <table>
-                <tr class="">
-                    <td><label>Name</label></td>
-                    <td><input class="border-2 border-black" type="te…
-[11:42 pm, 08/08/2023] Anu Fi: const reviveEntries = () => {
-    let entries = localStorage.getItem("user-Entries");
-    return entries ? JSON.parse(entries) : [];
-  };
-  
-  let nishaEntries = reviveEntries();
-  
-  const Entries = () => {
-    const entries = reviveEntries();
-  
-    const tableEntries = entries
-        .map(
-            (entry) => `<tr>
-                <td style = "padding: 2px 4px">${entry.name}</td>
-                <td style = "padding: 2px 4px">${entry.email}</td>
-                <td style = "padding: 2px 4px">${entry.password}</td>
-                <td style = "padding: 2px 4px">${entry.dob}</td>
-                <td style = "padding: 2px 4px">${entry.acceptedTermsAndConditions}</td>
-            </tr>`
-        )
-        .join("\n");
-  
-    const table = `<table>
-    …
+const userForm = document.getElementById("user-form");
+const userEntries = retrieveEntries();
+const details = document.getElementById("user-entries");
+
+
+function retrieveEntries() {
+  return JSON.parse(localStorage.getItem("UserEntries") || "[]");
+}
+
+function displayEntries() {
+  const tableEntries = userEntries
+    .map(({ name, email, password, dob, acceptTerms }) => `
+      <tr>
+        <td class="border px-4 py-2">${name}</td>
+        <td class="border px-4 py-2">${email}</td>
+        <td class="border px-4 py-2">${password}</td>
+        <td class="border px-4 py-2">${dob}</td>
+        <td class="border px-4 py-2">${acceptTerms}</td>
+      </tr>
+    `)
+    .join("\n");
+  const table = `
+    <table class="table-auto w-full">
+      <thead>
+        <tr>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Email</th>
+          <th class="px-4 py-2">Password</th>
+          <th class="px-4 py-2">Dob</th>
+          <th class="px-4 py-2">Accepted terms?</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableEntries}
+      </tbody>
+    </table>
+  `;
+
+  details.innerHTML = table;
+}
+
+function saveUserForm(event) {
+  event.preventDefault
+      
+    const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptTerms = document.getElementById("acceptTerms").checked;
+
+  const currentYear = new Date().getFullYear();
+  const birthYear = dob.split("-");
+  const year = birthYear[0];
+  const age = currentYear - year;
+
+
+  if (age < 18 || age > 55) {
+    document.getElementById("dob").style.border = "1px solid red";
+    return alert("You must be between 18 and 55 years old to register");
+  }
+
+  document.getElementById("dob").style.border = "none";
+
+  const entry = { name, email, password, dob, acceptTerms };
+  userEntries.push(entry);
+
+  localStorage.setItem("UserEntries", JSON.stringify(userEntries));
+  displayEntries();
+  userForm.reset();
+}
+
+userForm.addEventListener("submit", saveUserForm);
+displayEntries(); 
